d853831bba45c19c8c39e844e2f12d7bb2812cbc
Mohammed Ashraf <141479986+mohammedashraf16@users.noreply.github.com>
2024-03-14 00:47:07 +0200

    Create Notes ListView

--- lib/widgets/note_view_body.dart
+++ lib/widgets/note_view_body.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
-import 'package:notes_app/widgets/custom_note_item.dart';
+import 'package:notes_app/widgets/notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -15,10 +16,12 @@ class NotesViewBody extends StatelessWidget {
             height: 40,
           ),
           CustomAppBar(),
-          NoteItem(),
+          Expanded(child: NotesListView()),
         ],
       ),
     );
   }
 }
+
+
 
