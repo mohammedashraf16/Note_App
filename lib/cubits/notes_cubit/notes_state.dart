168a48a932bf5791e5ae8c1db60ca2a101a49c7d
mohammedashraf16 <mohammedashraf1692003@gmail.com>
2024-03-31 13:11:08 +0200

    Create Fetch Note Method

--- lib/cubits/notes_cubit/notes_state.dart
+++ lib/cubits/notes_cubit/notes_state.dart
@@ -3,12 +3,12 @@ part of 'notes_cubit.dart';
 @immutable
 sealed class NotesState {}
 
-final class AddNoteInitial extends NotesState {}
-final class AddNoteLoading extends NotesState {}
-final class AddNoteSuccess extends NotesState {
+final class NotesInitial extends NotesState {}
+final class NotesLoading extends NotesState {}
+final class NotesSuccess extends NotesState {
 final  List<NoteModel> notes ;
 
-  AddNoteSuccess(this.notes);
+  NotesSuccess(this.notes);
 }
 final class AddNoteFailuer extends NotesState {
   final String errorMessage;
